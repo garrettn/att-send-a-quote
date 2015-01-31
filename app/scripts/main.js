98e3cc98335487a27448bc98e60cff814b4aef05
Garrett Nay <garrettnay@ymail.com>
2015-01-31 09:51:24 -0700

    Get a random Charlie Sheen quote from Wikiquote

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+(function ($) {
+  var quoteText;
+  var quoteTitle = 'Charlie Sheen';
+
+  $(function () {
+    var $quote = $('[data-hook~=quote]');
+    var $title = $('[data-hook~=title]');
+
+    getNewQuote();
+
+    $('[data-hook~=new]').on('click', getNewQuote);
+    $('[data-hook~=send]').on('click', sendText);
+
+    function getNewQuote () {
+      WikiquoteApi.getRandomQuote(quoteTitle, function (quote) {
+        quoteText = quote.quote;
+        $quote.text(quote.quote);
+        $title.text(quote.titles);
+      });
+    }
+
+    function sendText () {
+      console.log('Sending text: ' + quoteText);
+    }
+
+  });
+
+})(jQuery);
